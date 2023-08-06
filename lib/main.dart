d851fe2c4dc5217d79650dbde09747fdccbbd8f0
anujbarve <anujbarve27@gmail.com>
2023-08-06 12:50:26 +0530

    added gitignore

--- lib/main.dart
+++ lib/main.dart
@@ -27,8 +27,7 @@ class MyApp extends StatelessWidget {
         // restart instead.
         //
         // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        // tested with just a hot reload
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
